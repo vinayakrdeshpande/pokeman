580242d316a9e166b366bf25c9ac402d41ebe48f
vinayakrdeshpande <15637546+vinayakrdeshpande@users.noreply.github.com>
2021-05-22 21:18:57 +0530

    intial commit

---
+++ src/main/java/com/game/pokeman/PokemanApplication.java
@@ -0,0 +1,13 @@
+package com.game.pokeman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/pokeman/PokemanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.pokeman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
